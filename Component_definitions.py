3436d9543e0e2e60863455b7b8f5ab29979971a1
JackHon55 <jack_hon55@hotmail.com>
2024-05-24 14:44:50 +1000

    implemented fwhm to sig conversion to allow for easier line definitions

--- Component_definitions.py
+++ Component_definitions.py
@@ -6,47 +6,31 @@ from PyQSOFit.PyQSOFit_SVL import LineDef, Section
 
 warnings.filterwarnings("ignore")
 
-hbo3_section = Section(section_name='O3', start_range=4500, end_range=6700)
+hbo3_section = Section(section_name='O3', start_range=4500, end_range=5300)
+hanl_section = Section(section_name='HA', start_range=6000, end_range=7000)
+
 line_hb_br1 = LineDef(l_name='Hb_br1', l_center=4861.33, scale=0.005, default_bel=True)
-line_hb_na = LineDef(l_name='Hb_na', l_center=4861.33, scale=0.002, sig=(1e-5, 0.001), voffset=1e-3, skew=(0,))
+line_hb_na = LineDef(l_name='Hb_na', l_center=4861.33, scale=0.002, fwhm=(50, 700), voffset=1e-3, skew=(0,))
 
 line_or = LineDef(l_name='OIII5007c', l_center=5006.843, scale=0.003, default_nel=True)
 line_ol = LineDef(l_name='OIII4959c', l_center=4958.91, flux_link='OIII5007c*0.33', profile_link='OIII5007c*1')
 
-hbo3_section.line_add(line_hb_br1)
-hbo3_section.line_add(line_hb_na)
-hbo3_section.line_add(line_or)
-hbo3_section.line_add(line_ol)
+hbo3_section.add_lines([line_hb_br1, line_hb_na, line_or, line_ol])
 
-hanl_section = Section(section_name='O3', start_range=4500, end_range=6700)
 line_ha_br1 = LineDef(l_name='Ha_br1', l_center=6562.82, scale=0.005, default_bel=True)
-line_ha_na = LineDef(l_name='Ha_na', l_center=6562.82, scale=0.002, sig=(1e-5, 0.001), voffset=1e-3, skew=(0,))
+line_ha_na = LineDef(l_name='Ha_na', l_center=6562.82, scale=0.002, fwhm=(50, 700), voffset=1e-3, skew=(0,))
 
 line_nr = LineDef(l_name='NII6585c', l_center=6583.46, scale=0.002, default_nel=True)
 line_nl = LineDef(l_name='NII6549c', l_center=6548.05, flux_link='NII6585c*0.33', profile_link='NII6585c*1')
 
-hanl_section.line_add(line_ha_br1)
-hanl_section.line_add(line_ha_na)
-hanl_section.line_add(line_nr)
-hanl_section.line_add(line_nl)
+hanl_section.add_lines([line_ha_br1, line_ha_na, line_nr, line_nl])
+
+noise_a = LineDef(l_name='noiA', l_center=4977, scale=-0.002, fwhm=(10, 350), voffset=1e-3, skew=(0,))
 
 newdata = np.concatenate([hbo3_section.lines, hanl_section.lines])
-
-# ------header-----------------
-hdr = fits.Header()
-hdr['lambda'] = 'Vacuum Wavelength in Ang'
-hdr['minwav'] = 'Lower complex fitting wavelength range'
-hdr['maxwav'] = 'Upper complex fitting wavelength range'
-hdr['ngauss'] = 'Number of Gaussians for the line. Not all functions supported for >1'
-hdr['sigval'] = 'Sigma information in strings, either a single value, [min, max], or name of line to tie to'
-hdr['voff  '] = 'Limits on velocity offset from the central wavelength [lnlambda]'
-hdr['iniskw'] = 'Initial guess of lineskew'
-hdr['fvalue'] = 'Relative scale factor for entries w/ same findex'
-# ------save line info-----------
-hdu = fits.BinTableHDU(data=newdata, header=hdr, name='data')
+hdu = fits.BinTableHDU(data=newdata, header=Section.hdu_generate(), name='data')
 
 # path definitions to save the component definitions file
 path1 = os.getcwd() + '/PyQSOFit/'  # the path of the source code file and qsopar.fits
-
-hdu.writeto(path1 + 'qsopar2.fits', overwrite=True)     # dont change this
+hdu.writeto(path1 + 'qsopar2.fits', overwrite=True)
 
--- PyQSOFit/PyQSOFit_SVL.py
+++ PyQSOFit/PyQSOFit_SVL.py
@@ -309,12 +309,13 @@ class InitialProfileParameters:
 
 
 class LineDef:
-    def __init__(self, sig: Tuple = (0, 0.01), profile_link: str = "", skew: Tuple = (-10, 10), gamma: float = 0,
+    def __init__(self, fwhm: Tuple = (100, 1000), profile_link: str = "", skew: Tuple = (-10, 10), gamma: str = "",
                  vmode: str = "", voffset: float = 0.0, scale: float = 0.005, flux_link: str = "",
                  default_bel: bool = False, default_nel: bool = False, *, l_name: str = "", l_center: float = 0.0):
         self.l_name = l_name
         self.l_center = l_center
-        self._sig = sig
+        self.fwhm = fwhm
+        self._sig = (0, 0.01)
         self.voffset = voffset
         self._skew = skew
         self.gamma = gamma
@@ -324,22 +325,24 @@ class LineDef:
         self.flux_link = flux_link
 
         if default_nel:
-            self._sig = (1e-5, 0.0017)
+            self.fwhm = (50, 1200)
             self.voffset = 1e-3
             self._skew = (0,)
         if default_bel:
-            self._sig = (0.0017, 0.01)
+            self.fwhm = (1200, 7000)
             self.voffset = 5e-3
             self._skew = (-10, 10)
 
     @property
     def sig(self):
-        if len(self._sig) == 2:
+        if len(self.fwhm) == 2:
+            self._sig = (self.fwhm_to_sig(self.fwhm[0]), self.fwhm_to_sig(self.fwhm[1]))
             return f"[{self._sig[0]}, {self._sig[1]}]"
         elif len(self._sig) == 1:
+            self._sig = (self.fwhm_to_sig(self.fwhm[0]),)
             return f"[{self._sig[0]}]"
         else:
-            print("Incorrect number of inputs for line sigma. Should be 1 (exact) or 2 (range) values")
+            print("Incorrect number of inputs for line fwhm. Should be 1 (exact) or 2 (range) values")
             print("Default to narrow line")
             return "[1e-5, 0.0017]"
 
@@ -369,8 +372,10 @@ class LineDef:
     def profile(self):
         l_gamma = ""
         l_voff = ""
-        if self.gamma != 0:
-            l_gamma = f"g[{self.gamma}];"
+        if self.gamma == "On":
+            l_gamma = f"g[];"
+        elif "f" in self.gamma:
+            l_gamma = self.gamma[1:]
         if l_voff != "":
             l_voff = f"v[{self.vmode}];"
 
@@ -382,6 +387,11 @@ class LineDef:
             return self.sig
         else:
             return self.plink
+
+    @staticmethod
+    def fwhm_to_sig(xfwhm: float):
+        c = 299792.458  # km/s
+        return np.round(np.sqrt(np.log(1 + ((xfwhm / c / 2.355) ** 2))), 5)
 
 
 class Section:
@@ -396,6 +406,10 @@ class Section:
         if self.start_range <= new_line.l_center <= self.end_range:
             self._lines.append(new_line)
 
+    def add_lines(self, lines: list):
+        for xline in lines:
+            self.line_add(xline)
+
     @property
     def lines(self):
         if self.rarray is None:
@@ -409,6 +423,19 @@ class Section:
     def linedef_to_tuple(self, xline: LineDef) -> Tuple:
         return (xline.l_center, self.section_name, self.start_range, self.end_range, xline.l_name,
                 xline.sig_info, xline.voffset, xline.profile, xline.scale)
+
+    @staticmethod
+    def hdu_generate() -> fits.Header:
+        hdr = fits.Header()
+        hdr['lambda'] = 'Vacuum Wavelength in Ang'
+        hdr['minwav'] = 'Lower complex fitting wavelength range'
+        hdr['maxwav'] = 'Upper complex fitting wavelength range'
+        hdr['ngauss'] = 'Number of Gaussians for the line. Not all functions supported for >1'
+        hdr['sigval'] = 'Sigma information in strings, either a single value, [min, max], or name of line to tie to'
+        hdr['voff  '] = 'Limits on velocity offset from the central wavelength [lnlambda]'
+        hdr['iniskw'] = 'Initial guess of lineskew'
+        hdr['fvalue'] = 'Relative scale factor for entries w/ same findex'
+        return hdr
 
 
 class QSOFit:
